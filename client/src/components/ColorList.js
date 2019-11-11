26241885b25047610376a5f021094cade96e67b5
silvodesigns <kevsilvo@gmail.com>
2019-11-10 22:00:44 -0500

    added delete request

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -27,7 +27,6 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
-    console.log(colorToEdit.id)
     axiosWithAuth().put(`http://localhost:5000/api/colors/${colorToEdit.id}`, colorToEdit)
     .then(res => {
       console.log(res)
@@ -40,6 +39,15 @@ const ColorList = ({ colors, updateColors }) => {
 
   const deleteColor = color => {
     // make a delete request to delete this color
+    axiosWithAuth().delete(`http://localhost:5000/api/colors/${color.id}`)
+    .then(res => {
+      console.log(res)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  
+
   };
 
   return (
