8985fd5c24ae6ec02e16160732d69e8d21bc73e6
silvodesigns <kevsilvo@gmail.com>
2019-11-10 20:32:38 -0500

    setting token and protected route

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,18 +1,19 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-import Bubbles from "./Bubbles";
-import ColorList from "./ColorList";
+// import Bubbles from "./Bubbles";
+// import ColorList from "./ColorList";
 
 const BubblePage = () => {
-  const [colorList, setColorList] = useState([]);
+  // const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
 
   return (
     <>
-      <ColorList colors={colorList} updateColors={setColorList} />
-      <Bubbles colors={colorList} />
+      {/* <ColorList colors={colorList} updateColors={setColorList} />
+      <Bubbles colors={colorList} /> */}
+      <h1>Welcome to BubblePage</h1>
     </>
   );
 };
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -6,7 +6,7 @@ const Login = () => {
   // when you have handled the token, navigate to the BubblePage route
   const [credentials, setCredentials] = useState({
       username: '',
-      password: '',
+      password: ''
   })
 
   const handleChange = (event) => {
@@ -14,14 +14,16 @@ const Login = () => {
       ...credentials,
       [event.target.name]: event.target.value
     })
+    console.log(credentials)
   }
 
   const handleSubmit = (e) => {
         e.preventDefault();
-        axios.post('http://localhost:5000', credentials)
+        axios.post('http://localhost:5000/api/login', credentials)
         .then( res =>  {
-          console.log(res.data)
-          localStorage.setItem("token", res.data.token)
+          console.log("hey")
+          localStorage.setItem("token", res.data.payload)
+          console.log(res)
         })
         .catch(err => {
           console.log(err)
@@ -31,10 +33,10 @@ const Login = () => {
     <>
       <h1>Welcome to the Bubble App!</h1>
       <p>Build a login page here</p>
-      <form>
+      <form onSubmit={handleSubmit}>
             <input type="username" name="username" placeholder="Username" value={credentials.username} onChange={handleChange} />
             <input type="password" name="password" placeholder="Password" value={credentials.password}  onChange={handleChange}/>
-            <button onSubmit={handleSubmit}>Log In</button>
+            <button >Log In</button>
 
 
       </form>
